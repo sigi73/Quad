b2bfe3ac05e1a33e94f63c69e84a28c6300fe1d5
Siddharth <siddharth.suri0@gmail.com>
2017-07-20 17:14:46 -0700

    Started working on modifying quad control to work from below the quad and send commands over mavros

--- ROS_Navigation/catkin_ws/src/quad_control/src/quad_control.cpp
+++ ROS_Navigation/catkin_ws/src/quad_control/src/quad_control.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "sensor_msgs/Image.h"
+#include "mavros_msgs/OverrideRCIn.h"
 
 #include <sstream>
 
@@ -10,6 +11,20 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <cmath>
 #include <iostream>
+
+#define TRANSLATION_THRESHOLD      10
+#define ROTATION_THRESHOLD         5  //Degrees
+#define HEIGHT_THRESHOLD           20
+
+#define STICK_CENTER          1500
+#define THROTTLE_START        1300
+#define THROTTLE_MIN          1000
+#define THROTTLE_MAX          2000
+
+#define ROLL_STICK_AMOUNT     500
+#define PITCH_STICK_AMOUNT    500
+#define YAW_STICK_AMOUNT      500
+#define THROTTLE_INCREMENT    5
 
 static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0)
 {
@@ -25,7 +40,7 @@ bool imageFound = false;
 
 void cameraCallback(const sensor_msgs::ImageConstPtr &msg)
 {
-  ROS_INFO("Camera_callback called");
+  //ROS_INFO("Camera_callback called");
   cv_bridge::CvImagePtr cv_ptr; 
   try
   {
@@ -83,7 +98,8 @@ int main(int argc, char **argv)
    * than we can send them, the number here specifies how many messages to
    * buffer up before throwing some away.
    */
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("quad_movement", 1000);
+  //ros::Publisher chatter_pub = n.advertise<std_msgs::String>("quad_movement", 1000);
+  ros::Publisher movement_pub = n.advertise<mavros_msgs::OverrideRCIn>("/mavros/rc/override", 1000);
   //ros::Subscriber sub = n.subscribe("picture_streamer", 1000, cameraCallback);
   ros::Subscriber sub = n.subscribe("/cv_camera/image_raw", 1000, cameraCallback);
 
@@ -107,18 +123,22 @@ int main(int argc, char **argv)
    */
   int count = 0;
 
+  float currentThrottle = THROTTLE_START;
+
   while (ros::ok())
   {
 
     cv::Mat src;
-    /*
-    cap >> src;
-    if (src.empty()) return -1;
-    */
     //ROS_INFO("Control loop");
     ros::spinOnce();
     if (!imageFound) continue;
+
     src = updated;
+    int width = src.size().width;
+    int height = src.size().height;
+    int xTarget = width / 2;
+    int yTarget = height / 2;
+    int angleTarget = 0;
 
 
     cv::Mat gray;
@@ -131,7 +151,7 @@ int main(int argc, char **argv)
     cv::findContours(bw.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
     std::vector<cv::Point> approx;
-    cv::Mat dst = src.clone();
+    //cv::Mat dst = src.clone();
 
     std::vector<cv::Point> triangleShape;
     std::vector<cv::Point> circleShape;
@@ -171,52 +191,74 @@ int main(int argc, char **argv)
 
     cv::Point2f paperCenter = cv::Point2f((circleCenter.x + triangleCenter.x) / 2.0f, (circleCenter.y + triangleCenter.y) / 2.0f);
 
+    
+    mavros_msgs::OverrideRCIn msg;
+    for (int i = 0; i < 8; i++)
+    {
+        msg.channels[i] = 65535;
+    }
+
+    float magnitude = sqrt(pow(triangleCenter.x - circleCenter.x, 2) + pow(triangleCenter.y - circleCenter.y, 2));
+    float quadAngle = acos((circleCenter.y - triangleCenter.y) / magnitude); //Reverse direction because comparing to vector (0, -1), straight up in an image
+    if (quadAngle < -ROTATION_THRESHOLD)
+    {
+      //Rotate right
+      float factor = quadAngle / 180.0f;
+      msg.channels[3] = STICK_CENTER + factor * YAW_STICK_AMOUNT;
+    }
+    else if (quadAngle > ROTATION_THRESHOLD)
+    {
+      //Rotate left
+      float factor = quadAngle / 180.0f;
+      msg.channels[3] = STICK_CENTER - factor * YAW_STICK_AMOUNT;
+    }
+    else
+    {
+      //Don't rotate
+      msg.channels[3] = STICK_CENTER;
+    }
+    
+    cv::Point2f forwardUnitVector = cv::Point2f((triangleCenter.x - circleCenter.x) / magnitude, (triangleCenter.y - circleCenter.y) / magnitude);
+    cv::Point2f rightUnitVector = cv::Point2f(-forwardUnitVector.y, forwardUnitVector.x);
+    cv::Point2f targetTranslation = cv::Point2f(xTarget - paperCenter.x, yTarget - paperCenter.y);
+    float targetTranslationMagnitude = sqrt(pow(targetTranslation.x, 2) + pow(targetTranslation.y, 2));
+
+    if (targetTranslationMagnitude > TRANSLATION_THRESHOLD)
+    {
+      //Roll control
+      msg.channels[0] = STICK_CENTER + (rightUnitVector.x * targetTranslation.x + rightUnitVector.y * targetTranslation.y) / targetTranslationMagnitude * ROLL_STICK_AMOUNT;
+
+      //Pitch control
+      msg.channels[1] = STICK_CENTER + (forwardUnitVector.x * targetTranslation.x + forwardUnitVector.y * targetTranslation.y) / targetTranslationMagnitude * PITCH_STICK_AMOUNT;
+    }
+    else
+    {
+      msg.channels[0] = STICK_CENTER;
+      msg.channels[1] = STICK_CENTER;
+    }
+
     float horizontalSpacing = 200;
-    float xTarget = (float)dst.cols / 2.0f;
-    float yTarget = (float)dst.rows / 2.0f;
-
-    std_msgs::String msg;
-    std::stringstream ss;
-
-    if (paperCenter.y > yTarget)
-    {    
-      ss << "-Move up-";
-    }
-    else if (paperCenter.y < yTarget)
-    {
-      ss << "-Move down-";
-    }
-
-    if (paperCenter.x > xTarget)
-    {
-      ss << "Move right";
-    }
-    else if (paperCenter.x < xTarget)
-    {
-      ss << "Move left";
-    }
-
     float dist = cv::norm(triangleCenter - circleCenter);
-    if (dist < horizontalSpacing)
+    if (dist < horizontalSpacing - HEIGHT_THRESHOLD)
     {
-      ss << "Move forward";
+      //Move down
+      currentThrottle -= THROTTLE_INCREMENT;
+      if (currentThrottle < THROTTLE_MIN) currentThrottle = THROTTLE_MIN;
     }
-    else if (dist > horizontalSpacing)
+    else if (dist > horizontalSpacing + HEIGHT_THRESHOLD)
     {
-      ss << "Move backward";
+      //Move up
+      currentThrottle += THROTTLE_INCREMENT;
+      if (currentThrottle > THROTTLE_MAX) currentThrottle = THROTTLE_MAX;
     }
+    else
+    {
+      //Maintain throttle
+    }
+    
+    msg.channels[2] = currentThrottle;
 
-    msg.data = ss.str();
-
-    //ROS_INFO("%s", msg.data.c_str());
-
-    /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
-     */
-    chatter_pub.publish(msg);
+    movement_pub.publish(msg);
 
 
     loop_rate.sleep();
